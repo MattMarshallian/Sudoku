3f0423f17f9ef9471820a83c3b146fd3a6bc9e27
Matt Marshall <matt.marshallian@gmail.com>
2016-12-18 19:24:36 +0100

    Przykładowe łatwe sudoku

--- Sudoku/Tests_Sudoku/TestValidation.cpp
+++ Sudoku/Tests_Sudoku/TestValidation.cpp
@@ -64,3 +64,61 @@ TEST(SudokuValidationRules, SudokuIsValidIfTheSameValuesInDifferSquare)
     sudoku.allpCells[62]->setValue(1);
     EXPECT_TRUE(sudoku.isValid());
 }
+
+
+TEST(SudokuValidationRules, RealSudokuFromInternet)
+{
+    /*
+    Very easy sudoku example
+
+    . . 8   . . .   7 . .
+    . . .   . . .   . . .
+    . . .   8 . 6   3 . .
+
+    1 . .   . . 7   9 . 5
+    . . 7   . 1 .   . 6 .
+    9 8 .   3 . 5   1 . .
+
+    . . 4   . 3 .   . . 8
+    . . 6   9 . 1   . . 4
+    7 . 1   . . .   . . .
+    */
+
+    SudokuTable sudoku;
+
+    sudoku.allpCells[2]->setValue(8);
+    sudoku.allpCells[6]->setValue(7);
+
+    sudoku.allpCells[21]->setValue(8);
+    sudoku.allpCells[23]->setValue(6);
+    sudoku.allpCells[24]->setValue(3);
+
+    sudoku.allpCells[27]->setValue(1);
+    sudoku.allpCells[32]->setValue(7);
+    sudoku.allpCells[33]->setValue(9);
+    sudoku.allpCells[35]->setValue(5);
+
+    sudoku.allpCells[38]->setValue(7);
+    sudoku.allpCells[40]->setValue(1);
+    sudoku.allpCells[43]->setValue(6);
+
+    sudoku.allpCells[45]->setValue(9);
+    sudoku.allpCells[46]->setValue(8);
+    sudoku.allpCells[48]->setValue(3);
+    sudoku.allpCells[50]->setValue(5);
+    sudoku.allpCells[51]->setValue(1);
+
+    sudoku.allpCells[56]->setValue(4);
+    sudoku.allpCells[58]->setValue(3);
+    sudoku.allpCells[62]->setValue(8);
+
+    sudoku.allpCells[65]->setValue(6);
+    sudoku.allpCells[66]->setValue(9);
+    sudoku.allpCells[68]->setValue(1);
+    sudoku.allpCells[71]->setValue(4);
+
+    sudoku.allpCells[72]->setValue(7);
+    sudoku.allpCells[74]->setValue(1);
+
+    EXPECT_TRUE(sudoku.isValid());
+}
